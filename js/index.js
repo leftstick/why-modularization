6ce613fd9341aba433327ed826e53e402c71b210
Howard.Zuo <leftstick@qq.com>
2015-09-16 15:08:08 +0800

    update commonjs demo

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 'use strict';
 
 require('bootstrap/dist/css/bootstrap.css');
-var Header = require('./logic/Header');
-var Content = require('./logic/Content');
-var Footer = require('./logic/Footer');
+var $ = require('jquery');
+var _ = require('lodash');
+var UserService = require('./logic/UserService');
+var usersTpl = require('./logic/usersTpl.html');
 
-console.log('document loaded');
-Header.create();
-Content.create();
-Footer.create();
-console.log('everything loaded');
+setTimeout(function() {
+    UserService.fetchUsers(Math.random() * 100)
+        .done(function(users) {
+            var html = _.template(usersTpl)({users: users});
+            $('.usersList').empty();
+            $('.usersList').append(html);
+            $('#info').toggleClass('alert-warning').toggleClass('alert-success').html('User List loaded');
+        });
+}, 1000);
